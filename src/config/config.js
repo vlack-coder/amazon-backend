f0185f29c166ea65bd6361d1f9c9f3ccf8bdc09c
vlack_coder <ajims119@gmail.com>
2021-02-04 15:32:36 +0000

    first commited

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   // mongo_url: "mongodb+srv://upbase:upbase@cluster0-g3vvd.mongodb.net/upbase?retryWrites=true&w=majority",
+  mongo_url: 'mongodb+srv://Ghimmy:Ghimmy@ghimmycluster.7gwtp.mongodb.net/Amazon?retryWrites=true&w=majority',
   mongo_url: "mongodb://localhost:27017/Amazon",
   mongo_test_url: "",
   secret: "upbaseultimtesecret"
